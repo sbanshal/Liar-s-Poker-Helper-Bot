3ea7df113a44823f9545519697d4a7bcea5f62f9
Shlok Banshal <shlok.banshal@duke.edu>
2025-07-23 20:53:05 -0400

    Flask uses the port Render gives it

--- server.py
+++ server.py
@@ -22,4 +22,5 @@ def upload():
         return jsonify({"status": "error", "message": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port)
